c0fd55bbd41777226c6d1492676d8e3e4ad8a1ee
markng <stngad@connect.ust.hk>
2023-04-02 22:01:44 +0800

    Implemented simple tree-walk of python ast

--- treewalk.py
+++ treewalk.py
@@ -1,10 +1,18 @@
+from operator import add, sub, mul
 import ast
 import unittest
 
 # Tree-Walk evaluation stimulation of AST from Python's `ast` module
 
 def evaluate(node):
-    return 0
+    if isinstance(node, ast.Constant):
+        return node.value
+    elif isinstance(node, ast.Expression):
+        return evaluate(node.body)
+    elif isinstance(node, ast.BinOp):
+        a = evaluate(node.left)
+        b = evaluate(node.right)
+        return { ast.Add: add, ast.Sub: sub, ast.Mult: mul }.get(type(node.op))(a, b)
 
 class TestEvaluate(unittest.TestCase):
     def test_evaluate(self):
@@ -17,7 +25,7 @@ class TestEvaluate(unittest.TestCase):
         for s, expected in test_cases:
             expr = ast.parse(s, mode='eval')
             # Uncomment to print AST for expression
-            # print(ast.dump(expr, indent=2))
+            print(ast.dump(expr, indent=2))
             actual = evaluate(expr)
             if actual != expected:
                 raise AssertionError(f'"{s}": expected {expected}, got {actual}')
